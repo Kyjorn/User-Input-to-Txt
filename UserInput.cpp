6cbeadbaac904e8d41e48e79f5be9f7955d25c89
Kyjron <hendonmshane@gmail.com>
2025-04-20 21:18:07 -0600

    Add C++ configurations for appending  CSC450

--- UserInput.cpp
+++ UserInput.cpp
@@ -1,14 +1,46 @@
 #include <iostream>;
+#include <fstream>;
+#include <string>;
+#include <algorithm>;
 using namespace std;
+
+//reverse method
 
 int main () {
 
     //variables
     string userIn;
-    string userFile;
-    string reverseUserFile;
+    string userFile = CSC450_CT5_mod5.txt;
+    string reverseUserFile = CSC450_CT5_mod5_reverse.txt; 
+
+    //gets userIn
+    cout << "Enter text to be appended in the file: ";
+    getline(cin, userIn);
+
+    //appends CSC450_CT5_mod5
+    ofstream outFile("CSC450_CT5_mod5.txt", ios::app);
+    if (outFile.is_open()) {
+        outFile << userIn << endl;
+        outFile.close();
+    } else {
+        cout << "Error opening file for appending.\n";
+        return 1;
+    }
+
+    //read and reverses content
+    ifstream inFile("CSC450_CT5_mod5.txt");
+    if (inFile.is_open()) {
+        string line;
+        while (getline(inFile, line)) {
+            reverse(line.begin(), line.end());
+            outFile << line << endl;
+        }
+        inFile.close();
+    } else {
+        cout << "Error opening file for reading.\n";
+        return 1;
+    }
 
 return 0;
 }
 
-//reverse method
